327362725836872e9fa61becc690bc0536fc2ecb
ben foden <ben.foden@gmail.com>
2024-07-02 16:03:31 +0900

    set reasonable limits

--- src/app/(session)/upgrade/page.tsx
+++ src/app/(session)/upgrade/page.tsx
@@ -10,50 +10,54 @@ export default function Upgrade() {
   const router = useRouter();
 
   return (
-    <div>
-      Checkout
-      <Card variant="form">
-        <form
-          onSubmit={async (event) => {
-            event.preventDefault(); // Prevent the default form submission
+    <div className="relative flex h-full w-full overflow-hidden">
+      <div className="z-20 flex h-dvh w-full items-center justify-center">
+        <div className="flex w-80 flex-col items-center justify-center text-xl">
+          <h1 className="mb-8 text-xl font-light">Checkout</h1>
+          <Card variant="form">
+            <form
+              onSubmit={async (event) => {
+                event.preventDefault(); // Prevent the default form submission
 
-            console.log(event.target);
-            try {
-              let period: "monthly" | "yearly" | undefined;
-              const target = event.nativeEvent as SubmitEvent;
-              const submitter = target.submitter as HTMLButtonElement;
-              if (submitter?.id === "monthly") {
-                period = "monthly";
-              } else if (submitter?.id === "yearly") {
-                period = "yearly";
-              }
-              if (!period) {
-                console.error("Payment period is not defined");
-                throw new Error("Payment period is not defined");
-              }
+                console.log(event.target);
+                try {
+                  let period: "monthly" | "yearly" | undefined;
+                  const target = event.nativeEvent as SubmitEvent;
+                  const submitter = target.submitter as HTMLButtonElement;
+                  if (submitter?.id === "monthly") {
+                    period = "monthly";
+                  } else if (submitter?.id === "yearly") {
+                    period = "yearly";
+                  }
+                  if (!period) {
+                    console.error("Payment period is not defined");
+                    throw new Error("Payment period is not defined");
+                  }
 
-              const { checkoutUrl } = await createCheckoutSession({
-                period,
-              });
+                  const { checkoutUrl } = await createCheckoutSession({
+                    period,
+                  });
 
-              console.log(checkoutUrl);
+                  console.log(checkoutUrl);
 
-              if (checkoutUrl) {
-                void router.push(checkoutUrl);
-              }
-            } catch (error) {
-              console.error(error);
-            }
-          }}
-        >
-          <Button type="submit" id="monthly">
-            Get monthly
-          </Button>
-          <Button type="submit" id="yearly">
-            Get yearly
-          </Button>
-        </form>
-      </Card>
+                  if (checkoutUrl) {
+                    void router.push(checkoutUrl);
+                  }
+                } catch (error) {
+                  console.error(error);
+                }
+              }}
+            >
+              <Button type="submit" id="monthly">
+                Get monthly
+              </Button>
+              <Button type="submit" id="yearly">
+                Get yearly
+              </Button>
+            </form>
+          </Card>
+        </div>
+      </div>
     </div>
   );
 }
