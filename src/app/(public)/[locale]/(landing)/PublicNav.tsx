eaebff8482533ce7648abce064a4f12a07dcc4d2
Ben Foden <ben.foden@gmail.com>
2024-07-10 22:25:45 +0900

    wip

--- src/app/(public)/[locale]/(landing)/PublicNav.tsx
+++ src/app/(public)/[locale]/(landing)/PublicNav.tsx
@@ -5,6 +5,7 @@ import { getServerAuthSession } from "~/server/auth";
 import LocaleSwitcher from "../../../_components/LocaleSwitcher";
 
 import dynamic from "next/dynamic";
+import DropDownMenu from "~/app/_components/DropDown";
 
 const SetThemeButton = dynamic(
   () => import("~/app/_components/ToggleTheme").then((mod) => mod.ThemeToggle),
@@ -19,12 +20,12 @@ export async function PublicNav() {
   const t = await getTranslations();
 
   return (
-    <nav className="z-100 flex w-full flex-wrap items-center justify-between bg-transparent pl-4 pt-4 sm:pr-4 sm:pt-0">
+    <nav className="z-100 w-full flex-wrap items-center justify-between bg-transparent pl-4 pt-4 sm:pr-4 sm:pt-0">
       <div className="flex items-center">
         <h1>
           <Link
             href="/"
-            className=" rounded-full px-4 py-2 text-xl font-light no-underline"
+            className="rounded-full px-4 py-2 text-xl font-light no-underline"
             aria-label="skydiary logo"
           >
             skydiary
@@ -43,25 +44,30 @@ export async function PublicNav() {
           {t("nav.pricing")}
         </Link>
       </div>
-      <div className="flex items-center gap-4 sm:pr-4">
-        {!session && (
-          <Link
-            href={session ? "/home" : "/auth/signin"}
-            className="text-nowrap rounded-full px-4 py-2 no-underline transition hover:bg-white/50 dark:bg-black/60"
-          >
-            {t("nav.login")}
+      <div className="hidden sm:block">
+        <div className="flex items-center gap-4 sm:pr-4">
+          {!session && (
+            <Link
+              href={session ? "/home" : "/auth/signin"}
+              className="text-nowrap rounded-full px-4 py-2 no-underline transition hover:bg-white/50 dark:bg-black/60"
+            >
+              {t("nav.login")}
+            </Link>
+          )}
+          <Link href={session ? "/home" : "/auth/signin"}>
+            <Button>
+              <span className="text-nowrap">
+                {session ? t("nav.home") : t("nav.signup")}
+              </span>
+            </Button>
           </Link>
-        )}
-        <Link href={session ? "/home" : "/auth/signin"}>
-          <Button>
-            <span className="text-nowrap">
-              {session ? t("nav.home") : t("nav.signup")}
-            </span>
-          </Button>
-        </Link>
-        <span> · </span>
-        <LocaleSwitcher />
-        <SetThemeButton />
+          <span> · </span>
+          <LocaleSwitcher />
+          <SetThemeButton />
+        </div>
+      </div>
+      <div className="block sm:hidden">
+        <DropDownMenu>hi</DropDownMenu>
       </div>
     </nav>
   );
