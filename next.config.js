4170579d561073bc9e00deaa4e87f15f9c64e6ca
ben foden <ben.foden@gmail.com>
2024-07-14 15:53:55 +0900

    more security stuff

--- next.config.js
+++ next.config.js
@@ -32,6 +32,22 @@ const config = {
           value:
             "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://va.vercel-scripts.com; img-src 'self'; style-src 'self' 'unsafe-inline'; object-src 'none';",
         },
+        {
+          key: "Strict-Transport-Security",
+          value: "max-age=63072000; includeSubDomains; preload",
+        },
+        {
+          key: "X-Frame-Options",
+          value: "DENY",
+        },
+        {
+          key: "X-Content-Type-Options",
+          value: "nosniff",
+        },
+        {
+          key: "Referrer-Policy",
+          value: "no-referrer",
+        },
       ],
     },
   ],
