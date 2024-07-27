3f3a5bc7be9dafaff0d15096bd22970a89762601
ben foden <ben.foden@gmail.com>
2024-07-27 19:25:40 +0900

    fix auth screen

--- src/app/(auth)/auth/new-user/page.tsx
+++ src/app/(auth)/auth/new-user/page.tsx
@@ -16,101 +16,99 @@ const NewUserPage: React.FC = async () => {
   // if (session.user.name) return redirect("/home");
 
   return (
-    <div className="relative flex h-full w-full overflow-hidden">
-      <div className="z-20 flex h-dvh w-full items-center justify-center">
-        <div className="flex w-full flex-col items-center justify-center sm:w-96">
-          <h1 className="mb-8 text-xl font-light">{t("new-user.title")}</h1>
-          <Card variant="form">
-            <p className="text-sm opacity-60">{t("settings.description")}</p>
-            <form
-              className="flex flex-col gap-4"
-              action={async (formData) => {
-                "use server";
-                const name: string = formData.get("name") as string;
-                const age = Number(formData.get("age"));
-                const gender: string = formData.get("gender") as string;
-                const isWorkFocused = formData.get("mainFocused") === "work";
+    <div className="flex h-full w-full flex-col items-center justify-center py-12">
+      <div className="flex w-full flex-col items-center justify-center sm:w-96">
+        <h1 className="mb-8 text-xl font-light">{t("new-user.title")}</h1>
+        <Card variant="form">
+          <p className="text-sm opacity-60">{t("settings.description")}</p>
+          <form
+            className="flex flex-col gap-4"
+            action={async (formData) => {
+              "use server";
+              const name: string = formData.get("name") as string;
+              const age = Number(formData.get("age"));
+              const gender: string = formData.get("gender") as string;
+              const isWorkFocused = formData.get("mainFocused") === "work";
 
-                const imageFile =
-                  (formData.get("imageFile") as File) ?? undefined;
-                const image = await getNewImageUrl({ imageFile });
-                const isUser = true;
+              const imageFile =
+                (formData.get("imageFile") as File) ?? undefined;
+              const image = await getNewImageUrl({ imageFile });
+              const isUser = true;
 
-                if (name) {
-                  try {
-                    // todo: add user locale to user profile
-                    await api.user.updateUser({ name, isWorkFocused });
-                    await api.persona.create({
-                      name,
-                      age: age ?? 0,
-                      gender: gender ?? "",
-                      traits: "",
-                      image,
-                      isUser,
-                    });
-                  } catch (error) {
-                    console.error("Error updating user:", error);
-                  }
-                  redirect("/home");
+              if (name) {
+                try {
+                  // todo: add user locale to user profile
+                  await api.user.updateUser({ name, isWorkFocused });
+                  await api.persona.create({
+                    name,
+                    age: age ?? 0,
+                    gender: gender ?? "",
+                    traits: "",
+                    image,
+                    isUser,
+                  });
+                } catch (error) {
+                  console.error("Error updating user:", error);
                 }
-              }}
-            >
-              <Input
-                id="name"
-                name="name"
-                label={t("new-user.yourName")}
-                required
-              />
-              <Input
-                type="radio"
-                required
-                radioOptions={[
-                  {
-                    id: "personal",
-                    label: t("new-user.personal"),
-                    value: "personal",
-                    checked: true,
-                  },
-                  { id: "work", label: t("new-user.work"), value: "work" },
-                ]}
-                name="mainFocus"
-                label={t("new-user.mainFocus")}
-              />
-              <Input
-                type="number"
-                id="age"
-                name="age"
-                label={t("new-user.your age")}
-                min={13}
-              />
+                redirect("/home");
+              }
+            }}
+          >
+            <Input
+              id="name"
+              name="name"
+              label={t("new-user.yourName")}
+              required
+            />
+            <Input
+              type="radio"
+              required
+              radioOptions={[
+                {
+                  id: "personal",
+                  label: t("new-user.personal"),
+                  value: "personal",
+                  checked: true,
+                },
+                { id: "work", label: t("new-user.work"), value: "work" },
+              ]}
+              name="mainFocus"
+              label={t("new-user.mainFocus")}
+            />
+            <Input
+              type="number"
+              id="age"
+              name="age"
+              label={t("new-user.your age")}
+              min={13}
+            />
 
-              <Input
-                type="text"
-                id="gender"
-                name="gender"
-                label={t("new-user.your identities")}
-                placeholder={t("new-user.placeholder identities")}
-              />
-              <Input
-                type="text"
-                id="occupation"
-                name="occupation"
-                label={t("new-user.occupation")}
-              />
-              <Input
-                type="file"
-                id="imageFile"
-                name="imageFile"
-                label={t("new-user.profilePicture")}
-                fileSelectButtonLabel={t("new-user.chooseImage")}
-              />
+            <Input
+              type="text"
+              id="gender"
+              name="gender"
+              label={t("new-user.your identities")}
+              placeholder={t("new-user.placeholder identities")}
+            />
+            <Input
+              type="text"
+              id="occupation"
+              name="occupation"
+              label={t("new-user.occupation")}
+            />
+            <Input
+              type="file"
+              id="imageFile"
+              name="imageFile"
+              label={t("new-user.profilePicture")}
+              fileSelectButtonLabel={t("new-user.chooseImage")}
+            />
 
-              <FormButton variant="submit">
-                {t("auth.save and continue")}
-              </FormButton>
-            </form>
-          </Card>
-        </div>
+            <FormButton variant="submit">
+              {t("auth.save and continue")}
+            </FormButton>
+          </form>
+        </Card>
       </div>
     </div>
   );
