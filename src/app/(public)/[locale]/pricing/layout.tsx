5bcdfdad9631c877e74626a611c9952308199c27
ben foden <ben.foden@gmail.com>
2024-07-28 13:34:21 +0900

    fix import error

--- src/app/(public)/[locale]/pricing/layout.tsx
+++ src/app/(public)/[locale]/pricing/layout.tsx
@@ -1,11 +1,34 @@
+import { type Metadata } from "next";
 import { getTranslations } from "next-intl/server";
 import DropDownUser from "~/app/_components/DropDownUser";
 import { NavChevronLeft } from "~/app/_components/NavChevronLeft";
 import { SessionNav } from "~/app/_components/SessionNav";
+import { type Locale } from "~/config";
 import { getServerAuthSession } from "~/server/auth";
 import { planFromId } from "~/utils/constants";
 import { PublicNav } from "../(landing)/PublicNav";
 import UpgradeBody from "./page";
+
+export async function generateMetadata({
+  params: { locale },
+}: {
+  params: { locale: Locale };
+}): Promise<Metadata> {
+  const t = await getTranslations({ locale, namespace: "metadata" });
+
+  return {
+    title: t("pricing.title"),
+    description: t("pricing.description"),
+    openGraph: {
+      title: t("pricing.title"),
+      description: t("pricing.description"),
+      url: "https://skydiary.app/pricing",
+      siteName: "skydiary",
+      locale,
+      type: "website",
+    },
+  };
+}
 
 export default async function Upgrade() {
   const t = await getTranslations();
