5b4ce0d59d02dd13621ed9e644c1430b4da71444
ben foden <ben.foden@gmail.com>
2024-08-01 11:32:07 +0900

    wip on privacy content

--- src/app/(public)/[locale]/(landing)/features/page.tsx
+++ src/app/(public)/[locale]/(landing)/features/page.tsx
@@ -1,5 +1,6 @@
 import { type Metadata } from "next";
 import { getTranslations } from "next-intl/server";
+import Link from "next/link";
 import { Card } from "~/app/_components/Card";
 import { type Locale } from "~/config";
 
@@ -26,12 +27,71 @@ export async function generateMetadata({
 
 export default async function Features() {
   const t = await getTranslations();
+
+  const features = [
+    {
+      title: t("features.feature1.title"),
+      description: t("features.feature1.description"),
+      imageSrc: "/images/feature1.png",
+    },
+    {
+      title: t("features.feature2.title"),
+      description: t.rich("features.feature2.description", {
+        privacy: (chunks) => (
+          <Link className="text-link" href="/features/privacy">
+            {chunks}
+          </Link>
+        ),
+      }),
+      imageSrc: "/images/feature2.png",
+    },
+    {
+      title: t("features.feature3.title"),
+      description: t("features.feature3.description"),
+      imageSrc: "/images/feature3.png",
+    },
+  ];
+  const miniFeatures = [
+    {
+      title: t("features.miniFeature1.title"),
+    },
+    {
+      title: t("features.miniFeature2.title"),
+    },
+    {
+      title: t("features.miniFeature3.title"),
+    },
+  ];
   return (
     <div className="flex w-full flex-col items-start justify-start gap-2 sm:max-w-[768px]">
       <h1 className="mb-4 text-3xl font-light">{t("features.title")}</h1>
-      <Card variant="textBlock" isButton={false}>
-        yo
-      </Card>
+      <section className="flex w-full flex-col items-start justify-start gap-16">
+        <h2 className="mb-4 text-6xl font-extralight">
+          {t("features.heroTitle")}
+        </h2>
+      </section>
+      <section className="mt-16 flex w-full flex-col items-start justify-start gap-16">
+        {features.map((feature, index) => (
+          <Card isButton={false} key={index}>
+            <div className="flex w-full flex-col items-start justify-start gap-4">
+              <h3 className="text-4xl font-extralight">{feature.title}</h3>
+              <p className="text-sm">{feature.description}</p>
+            </div>
+          </Card>
+        ))}
+      </section>
+      <section className="mt-16 flex w-full flex-col items-start justify-start gap-16">
+        <h2 className="mb-4 text-4xl font-extralight">
+          {t("features.miniFeaturesTitle")}
+        </h2>
+        <Card isButton={false}>
+          <ul className="ml-4 flex w-full list-disc flex-col items-start justify-start gap-2">
+            {miniFeatures.map((feature, index) => (
+              <li key={index}>{feature.title}</li>
+            ))}
+          </ul>
+        </Card>
+      </section>
     </div>
   );
 }
---
+++ src/app/(public)/[locale]/(landing)/privacy-pledge/page.tsx
@@ -0,0 +1,73 @@
+import { type Metadata } from "next";
+import { getTranslations } from "next-intl/server";
+import { Card } from "~/app/_components/Card";
+import { type Locale } from "~/config";
+
+export async function generateMetadata({
+  params: { locale },
+}: {
+  params: { locale: Locale };
+}): Promise<Metadata> {
+  const t = await getTranslations({ locale, namespace: "metadata" });
+
+  return {
+    title: t("privacyPledge.title"),
+    description: t("privacyPledge.description"),
+    openGraph: {
+      title: t("privacyPledge.title"),
+      description: t("privacyPledge.description"),
+      url: "https://skydiary.app/privacy-pledge",
+      siteName: "skydiary",
+      locale,
+      type: "website",
+    },
+  };
+}
+
+export default async function PrivacyPledged() {
+  const t = await getTranslations();
+
+  const privacyDetails = [
+    {
+      title: t("privacyPledge.detail1.title"),
+      description: t("privacyPledge.detail1.description"),
+    },
+    {
+      title: t("privacyPledge.detail2.title"),
+      description: t("privacyPledge.detail2.description"),
+    },
+    {
+      title: t("privacyPledge.detail3.title"),
+      description: t("privacyPledge.detail3.description"),
+    },
+    {
+      title: t("privacyPledge.detail4.title"),
+      description: t("privacyPledge.detail4.description"),
+    },
+    {
+      title: t("privacyPledge.detail5.title"),
+      description: t("privacyPledge.detail5.description"),
+    },
+  ];
+
+  return (
+    <div className="flex w-full flex-col items-start justify-start gap-2 sm:max-w-[768px]">
+      <h1 className="mb-4 text-3xl font-light">{t("privacyPledge.title")}</h1>
+      <section className="flex w-full flex-col items-start justify-start gap-16">
+        <p>{t("privacyPledge.description")}</p>
+      </section>
+      <ol className="mt-16 flex w-full flex-col items-start justify-start gap-16">
+        {privacyDetails.map((detail, index) => (
+          <Card isButton={false} key={index}>
+            <li className="list-decimal">
+              <div className="flex w-full flex-col items-start justify-start gap-4">
+                <h3 className="text-4xl font-extralight">{detail.title}</h3>
+                <p className="text-sm">{detail.description}</p>
+              </div>
+            </li>
+          </Card>
+        ))}
+      </ol>
+    </div>
+  );
+}
