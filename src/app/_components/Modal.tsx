7002eb0db9fd2a69ea27bac85cf101004acb4f9c
ben foden <ben.foden@gmail.com>
2024-08-24 07:35:08 +0900

    fix

--- src/app/_components/Modal.tsx
+++ src/app/_components/Modal.tsx
@@ -3,10 +3,12 @@ import { Card } from "~/app/_components/Card";
 
 export default function Modal({ children }: { children: React.ReactNode }) {
   return (
-    <div className="fixed inset-0 z-10 flex h-full w-full items-center justify-center overflow-hidden">
-      <div className="absolute inset-0 h-full w-full bg-black/60 backdrop-blur-sm"></div>
-      <div className="relative my-16 flex max-h-[768px] w-full flex-col items-start overflow-y-auto md:max-w-3xl">
-        <Card isButton={false}>{children}</Card>
+    <div className="fixed inset-0 z-10 flex h-full w-full items-center justify-center">
+      <div className="absolute inset-0 h-full w-full bg-black/60 backdrop-blur-xl"></div>
+      <div className="relative my-16 flex max-h-full w-full flex-col items-start overflow-y-auto p-2 md:max-w-3xl md:p-0">
+        <Card isButton={false} variant="form">
+          {children}
+        </Card>
       </div>
     </div>
   );
