1457fca41ae789a6c9fc54d369d79c5f8c30abee
ben foden <ben.foden@gmail.com>
2024-09-04 13:56:19 +0900

    remove clogs

--- src/app/(session)/Announcement.tsx
+++ src/app/(session)/Announcement.tsx
@@ -22,11 +22,9 @@ export default function Announcement({ blogPost }: { blogPost?: BlogPost }) {
 
   useEffect(() => {
     if (!blogPost?.content) return;
-    console.log("blogPost", blogPost);
 
     formatContent(blogPost?.content ?? "")
       .then((content) => {
-        console.log("content", content);
         setContent(content);
       })
       .catch((error) => {
